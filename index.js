d007d5e88ffb0505398c550e0588f5d207aef5e2
Mariajose <tienditacote@gmail.com>
2018-01-23 18:20:33 -0300

    creation of html + style in head css, section + obj in js (without content)

---
+++ index.js
@@ -0,0 +1,7 @@
+ 
+//arreglo para contener imagenes 
+ var imagenes = [
+  {ID; , img; , name;  medidas; filtro1; 'small'  } //información por imagen
+  {ID; , img; , name;  medidas; filtro1; 'mediun' } //información por imagen
+  {ID; , img; , name;  medidas; filtro1; 'large'  } //información por imagen   
+ ]
---
+++ readme.md
@@ -0,0 +1,13 @@
+# PROYECTO EDITORIA 
+### Editoria es una libreria que ofrece un filtro de imagenes 
+### que se necesite mostrar
+
+
+## El Reto
+
+## Metodologia de trabajo
+
+## Requerimientos técnicos
+
+## Levantamiento ambiente de desarrollo
+
