8341eb95af0cdd7d969898dd753291092632877f
= <natalbornoz@gmail.com>
2018-01-25 17:52:36 -0300

    creacion de función que redimensiona imagen

--- essets/js/app.js
+++ essets/js/app.js
@@ -1,1 +1,45 @@
-alert("hola");
+// 'use strict'
+//
+// //llamo al boton
+// const boton = document.getElementBy('option-samall');
+// //cuando se agregue el evento "click" que escuche:
+// boton.addEventListener('click', function(){
+// //crear espacio para alamacenar imagenes en dimesion pequeña (esto se hace en el div vacio que está en el html)
+// const contenido = document.getElementById('cont');
+//
+// })
+
+// document.getElementById("div")
+// .style.height="400px";
+//
+// document.getElementById("div")
+// .style.width="400px";
+
+//-------------------------------------
+// INICIO REDIMENDION IMAGEN
+function tamano(){
+document.getElementById("div")
+.style.height="400px";
+document.getElementById("div")
+.style.width="400px";
+}
+// FIN REDIMENDION IMAGEN
+//-------------------------------------
+//
+// $(document).ready(() => {
+//
+// });
+//
+// console.log(data);
+// // Variables que utilizaré
+// var small = $('small');
+// var medium =$('medium');
+// var large = $('large');
+//
+// $('#categorias').on('change',function(){
+//
+// var selection = $('categorias').val(){
+//
+// }
+//
+// })
