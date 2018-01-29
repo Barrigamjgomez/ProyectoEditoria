1c4107cdc623d3936d00c4027d909248b7fe4d63
Mariajose <tienditacote@gmail.com>
2018-01-29 17:50:59 -0300

    arreglo de carpetas

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,33 +1,14 @@
-var data =[
-
-{"nombre": "accesorio","color": "Blanca","foto": "assets/img/ollas7.jpg", "marca": "madenssa"},
-{"nombre": "accesorio","color": "Roja","foto": "assets/img/ollas7.jpg", "marca": "madenssa"},
-{"nombre": "accesorio","color": "Blanca","foto": "assets/img/ollas7.jpg", "marca": "madenssa"},
-{"nombre": "accesorio","color": "Azul","foto": "assets/img/ollas7.jpg", "marca": "madenssa"},
-{"nombre": "accesorio","color": "Fucsia","foto": "assets/img/ollas7.jpg", "marca": "madenssa"},
-
-{"nombre": "cuchillo","color": "Blanca","foto": "assets/img/cuchillo-1.jpg", "marca": "madenssa"},
-{"nombre": "cuchillo","color": "Negro","foto": "assets/img/cuchillo-2.jpg", "marca": "madenssa"},
-{"nombre": "cuchillo","color": "Rojo","foto": "assets/img/cuchillo-3.jpg", "marca": "madenssa"},
-{"nombre": "cuchillo","color": "Verde","foto": "assets/img/cuchillo-4.jpg", "marca": "madenssa"},
-{"nombre": "cuchillo","color": "Amarillo","foto": "assets/img/cuchillo-5.jpg", "marca": "madenssa"},
-{"nombre": "cuchillo","color": "Azul","foto": "assets/img/cuchillo-6.jpg", "marca": "madenssa"},
-
-{"nombre": "hornos","color": "Blanca","foto": "img/dana.jpg", "marca": "madenssa"},
-{"nombre": "hornos","color": "Blanca","foto": "img/dana.jpg", "marca": "madenssa"},
-{"nombre": "hornos","color": "Blanca","foto": "img/dana.jpg", "marca": "madenssa"},
-{"nombre": "hornos","color": "Blanca","foto": "img/dana.jpg", "marca": "madenssa"},
-{"nombre": "hornos","color": "Blanca","foto": "img/dana.jpg", "marca": "madenssa"},
-{"nombre": "hornos","color": "Blanca","foto": "img/dana.jpg", "marca": "madenssa"},
-
-{"nombre": "olla","color": "Blanca","foto": "img/dana.jpg", "marca": "madenssa"},
-{"nombre": "olla","color": "Blanca","foto": "img/dana.jpg", "marca": "madenssa"},
-{"nombre": "olla","color": "Blanca","foto": "img/dana.jpg", "marca": "madenssa"},
-{"nombre": "olla","color": "Blanca","foto": "img/dana.jpg", "marca": "madenssa"},
-{"nombre": "olla","color": "Blanca","foto": "img/dana.jpg", "marca": "madenssa"},
-{"nombre": "olla","color": "Blanca","foto": "img/dana.jpg", "marca": "madenssa"}
-
-
-]
- 
-//arreglo para contener imagenes 
+var data = [
+      {
+        'size' : 'small',
+        'picture' : 'essets/images/image1.jpg'
+      },
+      {
+        'size' : 'small',
+        'picture' : 'essets/images/image2.jpg'
+      },
+      {
+        'size' : 'small',
+        'picture' : 'essets/images/image3.jpg'
+      }
+    ]
