53433a7a251aeda7f87a3afc0bf9d22baef29615
Natalbornoz <natalbornoz@gmail.com>
2018-02-01 21:00:33 -0300

    para bajar cambios

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,35 @@
-
+//Eventos -----------------------------------------------------------
+//aqui creamos un evento el cúal con document... llamamos al boton()
+//que se encuentra en el input del html y con .EventListener le damos
+//el evento al boton con la funcion click, que quiere decir que al
+//momento de hacer click se ejecute el tamano.-
 document.getElementById("button").addEventListener("click", function(){
-	tamano();
+    tamano();
 });
 document.getElementById("button1").addEventListener("click", function(){
-	tamano2();
+    tamano2();
 });
 document.getElementById("button2").addEventListener("click", function(){
-	tamano3();
+    tamano3();
 });
-
-// INICIO REDIMENSION IMAGEN
+// FIN EVENTOS
+//--------------------------------------------------------------------
+//Funciónes
+//Redimensión de Imagenes
+//Aqui encontramos 3  funciones diferentes , las cúal con document,get...llamamos a la imagen
+//que está contenida en un ID dentro de un div en el html, la función redimensiona
+//la imagen afectando las medidas del ID original y entregando la nueva medida con el evento
+//que indicamos arriba
 function tamano(){
-document.getElementById("img-contenedor")
-.style.width="80%";
+    document.getElementById("img-contenedor")
+    .style.width="100%";
 }
-
 function tamano2(){
-document.getElementById("img-contenedor")
-.style.width="70%";
+    document.getElementById("img-contenedor")
+    .style.width="70%";
 }
-
 function tamano3(){
-document.getElementById("img-contenedor")
-.style.width="100%";
+    document.getElementById("img-contenedor")
+    .style.width="30%";
 }
 // FIN REDIMENSION IMAGEN
-//-------------------------------------
